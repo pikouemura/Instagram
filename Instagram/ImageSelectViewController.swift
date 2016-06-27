bb641ce7fbe8d41128ec802364998f6a8500e85f
宙生上村 <uemurachuusei@uemura-no-macbook-pro.local>
2016-06-27 15:11:32 +0900

    init

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by 上村 宙生 on 2016/06/27.
+//  Copyright © 2016年 huemura. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 上村 宙生 on 2016/06/27.
+//  Copyright © 2016年 huemura. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
